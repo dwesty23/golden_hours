bdae5e38abfabc68bb2e44940d4c1ccae541df1f
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-04-01 16:26:30 -0500

    Added outline material functionality to interactable abstract class

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -4,31 +4,37 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public abstract class Interactable : MonoBehaviour
 {
+    public Material defaultMaterial;
+    public Material outlineMaterial;
+    private SpriteRenderer spriteRenderer;
     private void Reset() 
     {
         GetComponent<BoxCollider2D>().isTrigger = true;
     }
 
-    private OutlineEffect outlineEffect;
-
     public abstract void Interact();
 
     private void Awake()
     {
-        // Get the OutlineEffect component
-        outlineEffect = GetComponent<OutlineEffect>();
-        Debug.Log(outlineEffect);
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+        spriteRenderer.material = defaultMaterial;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Sophie"))
         {
             Debug.Log("Sophie is near the object. Press E to interact.");
-            Debug.Log(outlineEffect);
-            if (outlineEffect != null)
+            if (spriteRenderer == null)
             {
-                outlineEffect.enabled = true;
+                Debug.LogError("SpriteRenderer is null");
+                return;
             }
+            if (outlineMaterial == null)
+            {
+                Debug.LogError("OutlineMaterial is null");
+                return;
+            }
+            spriteRenderer.material = outlineMaterial;     
         }
     }
     
@@ -37,11 +43,7 @@ public abstract class Interactable : MonoBehaviour
         if (collision.gameObject.tag == "Sophie")
         {
             Debug.Log("Sophie is no longer near the object.");
-            Debug.Log(outlineEffect);
-            if (outlineEffect != null)
-            {
-                outlineEffect.enabled = false;
-            }
+            spriteRenderer.material = defaultMaterial;
         }
     }
 }
