62c7a3a47a055e220f13ac00d6473545a0d317d0
bhavik-goplani <56516858+bhavik-goplani@users.noreply.github.com>
2024-04-12 10:27:03 -0500

    Diner finish cut scene dialogue

---
+++ Assets/Scripts/MayaDinerCutSceneInteract.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MayaDinerCutSceneInteract : Interactable
+{
+    public Conversation convo;
+    private bool hasInteracted = false;
+
+    public override void Interact()
+    {
+        if (hasInteracted) return;
+        TriggerDialogue();
+    }
+
+    private void TriggerDialogue()
+    {
+        Debug.Log("Starting Dialogue");
+        hasInteracted = true;
+        DialogueManagerM.StartConversation(convo);
+        //Load next scene after dialogue
+        StartCoroutine(LoadSceneAfterDialogue());
+    }
+
+    private IEnumerator LoadSceneAfterDialogue()
+    {
+        // Wait until the dialogue is not active
+        while (!DialogueManagerM.IsConversationFinished())
+        {
+            yield return null;
+        }
+
+        // Load the scene
+        SceneManager.LoadScene("DinerOpenMain");
+    }
+}
