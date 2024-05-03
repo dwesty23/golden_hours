5b88fd5c964903cfccb34522312826d02cb4f9d6
Jacob Horton <99301327+hortojac@users.noreply.github.com>
2024-05-02 19:34:59 -0500

    credits cutscene

---
+++ Assets/Scripts/PlayCredits.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
+public class PlayCredits : MonoBehaviour
+{
+    [Header("Scene to Load")]
+    [SerializeField] private SceneField _SceneMainMenu;
+    public GameObject credits;
+    public GameObject credits2;
+    public Button mainMenuButton;
+    public Image mainMenuButtonImage;
+   
+    public void Start()
+    {
+        mainMenuButton.enabled = false;
+        mainMenuButtonImage.enabled = false;
+        StartCoroutine(ShowCredits());
+    }
+
+    public IEnumerator ShowCredits()
+    {
+        yield return new WaitForSeconds(2f);
+        while (credits.transform.position.y < 53f)
+        {
+            credits.transform.position += new Vector3(0, 1, 0) * 1.5f * Time.deltaTime;
+            credits2.transform.position += new Vector3(0, 1, 0) * 1.5f * Time.deltaTime;
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.5f);
+        mainMenuButton.enabled = true;
+        mainMenuButtonImage.enabled = true;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(_SceneMainMenu, LoadSceneMode.Single);
+    }
+}
